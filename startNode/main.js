12656dc71bdb0a299ee1c47e6304da208ce9edf6
kawtharjmaiel <kawthar.jmaiel@softtodo.com>
2023-03-16 13:45:43 +0100

    start

--- startNode/main.js
+++ startNode/main.js
@@ -1,10 +1,50 @@
 // console.log("Hello, World!");
 
-var http = require("http");
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.end('Hello World\n');
-         }).listen(8081);
+// var http = require("http");
+// http.createServer(function (request, response) {
+//     response.writeHead(200, {'Content-Type': 'text/plain'});
+//         response.end('Hello World\n');
+//          }).listen(8081);
           
-           // Console will print the message
-            console.log('Server running at http://127.0.0.1:8081/');
+//            // Console will print the message
+//             console.log('Server running at http://127.0.0.1:8081/');
+
+//Import events module
+var events = require('events');
+
+// Create an eventEmitter object
+var eventEmitter = new events.EventEmitter();
+
+// Create an event handler as follows
+var connectHandler = function connected() {
+   console.log('connection succesful.');
+  
+   // Fire the data_received event 
+   eventEmitter.emit('data_received');
+}
+
+// Bind the connection event with the handler
+eventEmitter.on('connection', connectHandler);
+ 
+// Bind the data_received event with the anonymous function
+eventEmitter.on('data_received', function() {
+   console.log('data received succesfully.');
+});
+
+// Fire the connection event 
+eventEmitter.emit('connection');
+
+console.log("Program Ended.");
+
+
+
+var fs = require("fs");
+
+fs.readFile('input.txt', function (err, data) {
+   if (err) {
+      console.log(err.stack);
+      return;
+   }
+   console.log(data.toString());
+});
+console.log("Program Ended");
